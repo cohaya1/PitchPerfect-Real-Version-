74183b28954b4465155fab9d270dea11aad8d55c
cohaya1 <chika.ohaya@gmail.com>
2020-02-02 21:48:27 -0500

    Fixed UI and modified added configure function for recording

--- PitchPerfect/RecordSoundViewController.swift
+++ PitchPerfect/RecordSoundViewController.swift
@@ -27,9 +27,9 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
     
 
     @IBAction func RecordButton(_ sender: Any) {
-        print("record button was pressed")
-        TapToRecord.text = "Recording in Progress"
-        StopRecordingButton.isEnabled = true
+       // print("record button was pressed")
+       // TapToRecord.text = "Recording in Progress"
+        //StopRecordingButton.isEnabled = true
         let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
@@ -43,19 +43,25 @@ class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.delegate = self
         audioRecorder.prepareToRecord()
         audioRecorder.record()
+        return configureUI(isRecording: true)
     }
-    
+    func configureUI(isRecording: Bool) {
+        StopRecordingButton.isEnabled = isRecording // to disable the button
+        RecordButton.isEnabled = !isRecording // to enable the button
+        TapToRecord.text = !isRecording ? "Tap to Record" : "Recording in Progress"
+
+    }
     
     @IBAction func StopRecording(_ sender: AnyObject) {
         print("Stop Recording button was pressed")
-        TapToRecord.text = "Tap to Record"
-         StopRecordingButton.isEnabled = false
+    //    TapToRecord.text = "Tap to Record"
+      //   StopRecordingButton.isEnabled = false
        audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         
         try! audioSession.setActive(false)
       
-        
+       return configureUI(isRecording: false)
         
         
     }
